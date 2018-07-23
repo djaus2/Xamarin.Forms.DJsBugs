7feb71c4c69c7f460c91edbe412bd7c26f7cd891
David Jones <davidjones@sportronics.com.au>
2018-07-23 20:20:16 +1000

    First upload

---
+++ ListViewBug/XamlTest2/XamlTest2/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamlTest2.Models;
+
+namespace XamlTest2.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ NoToolbarBug/XamarinTest3/XamarinTest3/Views/ItemsPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XamarinTest3.Models;
+using XamarinTest3.Views;
+using XamarinTest3.ViewModels;
+
+namespace XamarinTest3.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new ItemsViewModel();
+        }
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            var item = args.SelectedItem as Item;
+            if (item == null)
+                return;
+
+            var pg = new ItemDetailPage(new ItemDetailViewModel(item));
+
+            await Navigation.PushAsync(pg);
+
+            // Manually deselect item.
+            ItemsListView.SelectedItem = null;
+        }
+
+        async void AddItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.Items.Count == 0)
+                viewModel.LoadItemsCommand.Execute(null);
+        }
+
+        private void ItemsListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var zx = e.Item;
+        }
+    }
+}
